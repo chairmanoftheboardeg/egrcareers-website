99061231d6808c525af8aba1f1e6e7fb41ca7529
Tyler Nicholas <chairman@emiratesgrouproblox.link>
2025-09-28 07:25:24 +0400

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,3 +1,6 @@
+// Supabase client (project: whujzazokxignhvynqfp)
 const SUPABASE_URL = "https://whujzazokxignhvynqfp.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndodWp6YXpva3hpZ25odnlucWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwMTI0NzUsImV4cCI6MjA3NDU4ODQ3NX0.X9JvoKv9OKLMei7vHNDYW5mx-EPG_dUGj_XoH429-Yw";
+
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
