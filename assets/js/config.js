6f1bcadfa1de0caabd5b12aa343406f38e5e8cfb
Tyler Nicholas <chairman@emiratesgrouproblox.link>
2026-01-13 06:34:18 +0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.EGR_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT_REF.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
-  FUNCTIONS_BASE: "https://YOUR_PROJECT_REF.supabase.co/functions/v1",
-  HR_HEADER_NAME: "X-HR-PASSWORD"
+  SUPABASE_URL: "https://dvcbbvvxbpxzpxaadoco.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2Y2JidnZ4YnB4enB4YWFkb2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNjQ4NTAsImV4cCI6MjA4MzY0MDg1MH0.gzHjXYOts3Af2iXRPRT4KMnS7geimB0Z6BQyqojtu3w",
+  FUNCTIONS_BASE: "https://dvcbbvvxbpxzpxaadoco.supabase.co/functions/v1",
+  HR_HEADER_NAME: "EmiratesGroupRoblox#HRACCESS@2026"
 };
